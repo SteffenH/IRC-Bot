3669a97fc518d68fe9c28388ad51efd17673fbae
Steffen Heimrath <Steffenheimrath@gmail.COM>
2011-05-26 21:51:53 +0200

    initial release. connecting irc is working, sending email is working, sqlite3 connection is working

---
+++ mail.py
@@ -0,0 +1,40 @@
+# source: http://www.blog.pythonlibrary.org/2010/05/14/how-to-send-email-with-python/
+
+import os
+import smtplib
+ 
+from email import Encoders
+from email.MIMEBase import MIMEBase
+from email.MIMEMultipart import MIMEMultipart
+from email.Utils import formatdate
+ 
+filePath = r'D:\\Desktop\\IRC Bot\\README'
+ 
+def sendEmail(TO="bla@blub.de",
+              FROM="blub@bla.de"):
+    HOST = "smtp.web.de"
+ 
+    msg = MIMEMultipart()
+    msg["From"] = FROM
+    msg["To"] = TO
+    msg["Subject"] = "You've got mail!"
+    msg['Date']    = formatdate(localtime=True)
+ 
+    # attach a file
+    part = MIMEBase('application', "octet-stream")
+    part.set_payload( open(filePath,"rb").read() )
+    Encoders.encode_base64(part)
+    part.add_header('Content-Disposition', 'attachment; filename="%s"' % os.path.basename(filePath))
+    msg.attach(part)
+ 
+    server = smtplib.SMTP(HOST)
+    server.login(USERNAME, PASSWORD)  # optional
+ 
+    try:
+        failed = server.sendmail(FROM, TO, msg.as_string())
+        server.close()
+    except Exception, e:
+        errorMsg = "Unable to send email. Error: %s" % str(e)
+ 
+if __name__ == "__main__":
+    sendEmail()
---
+++ simplebot.py
@@ -0,0 +1,103 @@
+import sys
+import socket
+import string
+from os.path import isfile
+from mail import sendEmail
+from db import *
+
+if __name__ == "__main__":
+
+	#general settings
+	HOST="irc.freenode.net"
+	PORT=6667
+	NICK="MauBot"
+	IDENT="maubot"
+	REALNAME="MauritsBot"
+	readbuffer=""
+	# password for killing daemon via irc
+	PASSWORD="quit"
+	#options for commandline
+	
+	# DEAMON switch
+	
+	## IRC
+	# server, HOST
+	# PORT
+	# NICK
+	# IDENT
+	# REALNAME
+	# irc channel
+	
+	## MAIL
+	# email HOST
+	# email PORT
+	# username
+	# password
+	
+	## DB
+	#
+	DATABASE = "example.db"
+	if not isfile(DATABASE):
+		createTable(DATABASE)
+	
+	#connecting to server and sending appropriate orders 
+	#registering the nick and joining a channel
+	irc=socket.socket( socket.AF_INET, socket.SOCK_STREAM )
+	irc.connect((HOST, PORT))
+	irc.send("NICK %s\r\n" % NICK)
+	irc.send("USER %s %s bla :%s\r\n" % (IDENT, HOST, REALNAME))
+	irc.send("JOIN #irclib\r\n")
+
+	#s.send("QUIT\r\n")
+
+	while 1:
+		readbuffer=readbuffer + irc.recv(1024)
+		temp=string.split(readbuffer, "\n")
+		readbuffer=temp.pop()
+		
+		for line in temp:
+			line=string.rstrip(line)
+			line=string.split(line)
+			
+			
+			
+			#keep-alive messages
+			if(line[0]=="PING"):
+				irc.send("PONG %s\r\n" % line[1])
+			
+			# username-format: 
+			# :NICK!~IDENT@NETWORK
+			
+			if ((line[1] == "PART") | (line[1] == "JOIN")):
+				addEntry(DATABASE, line[1], line[0].split("!")[0][1:], line[2])
+				print line
+
+				
+			#received private message
+			elif ((line[1] == "PRIVMSG") & (line[2] == NICK)):
+				addEntry(DATABASE, line[1], line[0].split("!")[0][1:], " ".join(line[3:]))
+				print line
+				
+				#
+				# ORDERS
+				#
+				
+				#SEND adress adress ...
+				# sends the log to the given adresses
+				if (line[3][1:] == "SEND"):
+					for address in line[4:]:
+						#sendEmail(TO=address)
+						irc.send("PRIVMSG %s :Sending log to %s\r\n" % (line[0].split("!")[0][1:], address))
+				
+				#SEEN person person ...
+				# tells when persons where last seen in chatroom
+				elif (line[3][1:] == "SEEN"):
+					for person in line[4:]:
+						irc.send("PRIVMSG %s :%s was last seen %s\r\n" % (line[0].split("!")[0][1:], person, lastSeen(DATABASE, person)))
+			
+				elif (line[3][1:] == "QUIT"):
+					if len(line) > 4:
+						if line[4] == PASSWORD:
+							irc.send("PRIVMSG %s :Bye Bye\r\n" % line[0].split("!")[0][1:])
+							irc.send("QUIT\r\n")
+							exit()
